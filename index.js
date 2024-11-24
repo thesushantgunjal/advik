628d0d5a9ba58fd2fa30ab50dd3678220a2a00a5
Sushant Gunjal <101082819+thesushantgunjal@users.noreply.github.com>
2024-11-25 00:52:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -21,4 +21,7 @@ app.use("/",userroute);
 app.use("/admin",adminroute);
 
 
-app.listen(1000);
+const PORT = process.env.PORT || 3000; // Use environment variable or fallback to 3000
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running on http://0.0.0.0:${PORT}`);
+});
